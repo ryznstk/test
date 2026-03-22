4b6708978a4a64147e409cbd79e84174c15aae0c
BLU.rzyn <agusmhrd1k4@gmail.com>
2026-03-23 06:19:42 +0700

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -84,7 +84,8 @@ BOARD_USES_QCOM_HARDWARE := true
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
-    hardware/xiaomi/vintf/xiaomi_framework_compatibility_matrix.xml
+    hardware/xiaomi/vintf/xiaomi_framework_compatibility_matrix.xml \
+    $(DEVICE_PATH)/configs/hidl/peridot_framework_matrix.xml
 
 DEVICE_FRAMEWORK_MANIFEST_FILE := $(DEVICE_PATH)/configs/hidl/framework_manifest.xml
 DEVICE_MATRIX_FILE := hardware/qcom-caf/common/compatibility_matrix.xml
@@ -123,7 +124,6 @@ TARGET_KERNEL_SOURCE := kernel/xiaomi/sm8635
 KERNEL_LTO := thin
 TARGET_KERNEL_CONFIG := \
     gki_defconfig \
-    vendor/extra.config \
     vendor/pineapple_GKI.config \
     vendor/peridot_GKI.config
 
