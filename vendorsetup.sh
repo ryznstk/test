94e523b904af0030c6f4e9e04f58baa28019f9a2
BLU.rzyn <agusmhrd1k4@gmail.com>
2026-04-09 10:24:53 +0700

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -67,12 +67,17 @@ git reset --hard FETCH_HEAD
 croot
 
 cd system/sepolicy
-git fetch https://github.com/ryznstk/lunaris_system_sepolicy.git test
+git fetch https://github.com/ryznstk/lunaris_system_sepolicy.git 16.2
 git reset --hard FETCH_HEAD
 croot
 
 cd packages/apps/Settings
 git fetch https://github.com/ryznstk/packages_apps_Settings 16.2
+git reset --hard FETCH_HEAD
+croot
+
+cd frameworks/base
+git fetch https://github.com/Lunaris-AOSP/frameworks_base test
 git reset --hard FETCH_HEAD
 croot
 
